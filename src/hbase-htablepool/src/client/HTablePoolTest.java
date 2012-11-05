c69cf0ac4d2bd08f895fa4241114af940bc4acd8
Scott Miao <scott_miao@trend.com.twa>
2012-11-05 22:16:44 +0800

    1. call HTablePool.close and put methods in the finally 2. add handler for detecting all threads completed and then close the pool

--- src/hbase-htablepool/src/client/HTablePoolTest.java
+++ src/hbase-htablepool/src/client/HTablePoolTest.java
@@ -15,7 +15,8 @@ import org.apache.hadoop.hbase.util.Bytes;
 
 public class HTablePoolTest {
 	
-	
+	private static boolean readsDone;
+	private static boolean writesDone;
 	
 	public static void main(String[] args) {
 		HTablePool pool = new HTablePool();
@@ -35,69 +36,95 @@ public class HTablePoolTest {
 		case 1:
 			tableName = "spam_domain_table";
 			doRunReadThreads(pool, tableName, true);
-			pool.closeTablePool(tableName);
 			break;
 		case 2:
+			tableName = "usertable_1";
 			doRunWriteThreads(pool);
-			pool.closeTablePool("usertable_1");
+			readsDone = true; //no reads, set it done directly
 			break;
 		case 3:
 			tableName = "usertable_1";
 			doRunReadThreads(pool, tableName, false);
 			doRunWriteThreads(pool);
-			pool.closeTablePool(tableName);
 			break;
 		default:
 			System.err.println("arg:" + arg + " is undefined !!");
 			System.exit(1);
 		}
+		try{} finally {
+			while(! readsDone) { 
+				/* wait for all threads done*/ 
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+					throw new RuntimeException(e);
+				}
+			}
+			pool.closeTablePool(tableName);
+		}
 	}
 
 	private static void doRunReadThreads(HTablePool pool, String tableName, boolean check) {
-		for(int a = 0; a < 500; a++) {
-			new Thread(new ReadHTableThread(pool, tableName, check), "r" + a).start();
+		int totalNum = 500;
+		ThreadsCompleteHandler handler = new ThreadsCompleteHandler(
+				totalNum, ThreadsCompleteHandler.Type.READ);
+		for(int a = 0; a < totalNum; a++) {
+			new Thread(new ReadHTableThread(pool, tableName, check, handler), "r" + a).start();
 		}
 	}
 
 	private static void doRunWriteThreads(HTablePool pool) {
 		// run threads
 		String tableName = "usertable_1";
-		for(int a = 0; a < 500; a++) {
-			new Thread(new WriteHTableThread(pool, tableName, "cf1", "col1"), "w" + a).start();
+		int totalNum = 500;
+		ThreadsCompleteHandler handler = new ThreadsCompleteHandler(
+				totalNum, ThreadsCompleteHandler.Type.WRITE);
+		for(int a = 0; a < totalNum; a++) {
+			new Thread(new WriteHTableThread(pool, tableName, "cf1", "col1", handler), "w" + a).start();
+		}
+		
+		while(! writesDone) { 
+			/* wait for write threads done*/
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
 		}
 		
 		// confirm result
 		System.out.println("confirm put data");
 		HTableInterface table = pool.getTable(tableName);
+		ResultScanner scanner = null;
 		Scan scan = new Scan();
 		int a = 0;
 		try {
-			Thread.sleep(10000);
-			
 			a = 0;
-			ResultScanner scanner = table.getScanner(scan);
+			scanner = table.getScanner(scan);
 			Iterator<Result> it = scanner.iterator();
 			
 			while(it.hasNext()) {
 				it.next(); a++;
 			}
-			scanner.close();
-			pool.putTable(table);
 			
-			if(a == 500) {
-				System.out.println("write result: a:" + a + " == 500");
+			if(a == totalNum) {
+				System.out.println("write result: a:" + a + " == " + totalNum);
 			} else {
-				throw new IllegalStateException("a:" + a + " != 500");
+				throw new IllegalStateException("a:" + a + " != " + totalNum);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new RuntimeException(e);
+		} finally {
+			if(null != scanner) scanner.close();
+			pool.putTable(table);
 		}
 		
 		// clean test data
 		System.out.println("clean test data");
 		table = pool.getTable(tableName);
-		ResultScanner scanner;
 		List<Delete> deletes = new ArrayList<Delete>();
 		Delete delete = null;
 		try {
@@ -112,36 +139,39 @@ public class HTablePoolTest {
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new RuntimeException(e);
+		} finally {
+			if(null != scanner) scanner.close();
+			pool.putTable(table);
 		}
-		pool.putTable(table);
 	}
 	
 	static class ReadHTableThread implements Runnable {
 		private HTablePool pool;
 		private String tableName;
 		private boolean check;
-		public ReadHTableThread(HTablePool pool, String tableName) {
-			this(pool, tableName, true);
-		}
+		private ThreadsCompleteHandler handler;
 		
-		public ReadHTableThread(HTablePool pool, String tableName, boolean check) {
+		public ReadHTableThread(HTablePool pool, String tableName, boolean check, 
+				ThreadsCompleteHandler handler) {
 			this.pool = pool;
 			this.tableName = tableName;
 			this.check = check;
+			this.handler = handler;
 		}
+		
 		@Override
 		public void run() {
 			String tName = Thread.currentThread().getName();
 			System.out.println("Start tName:" + tName);
 			HTableInterface table = this.pool.getTable(this.tableName);
 			Scan scan = new Scan();
+			ResultScanner scanner = null;
 			try {
-				ResultScanner scanner = table.getScanner(scan);
+				scanner = table.getScanner(scan);
 				Iterator<Result> it = scanner.iterator();
-				Result rs = null;
 				int a = 0;
 				while(it.hasNext()) {
-					rs = it.next();
+					it.next();
 					a++;
 				}
 				if(this.check && 500 != a)
@@ -151,8 +181,38 @@ public class HTablePoolTest {
 			} catch (Exception e) {
 				e.printStackTrace();
 				throw new RuntimeException(e);
+			} finally {
+				if(null != scanner) scanner.close();
+				this.pool.putTable(table);
+				this.handler.incrementCompletedThread();
 			}
-			this.pool.putTable(table);
+		}
+	}
+	
+	static class ThreadsCompleteHandler {
+		private int totalCount;
+		private Type type;
+		private int completedThreads;
+		public static enum Type {READ, WRITE}
+		
+		public ThreadsCompleteHandler(int totalCount, Type type) {
+			this.totalCount = totalCount;
+			this.type = type;
+		}
+
+		public void incrementCompletedThread() {
+			this.completedThreads++;
+			if(this.completedThreads >= this.totalCount) {
+				if(Type.READ == this.type) {
+					readsDone = true;
+					System.out.println("readsDone:" + readsDone);
+				} else {
+					writesDone = true;
+					System.out.println("writesDone:" + writesDone);
+				}
+			}
+			System.out.println("amount of completedThreads:" + this.completedThreads + 
+					" for type:" + this.type);
 		}
 	}
 	
@@ -161,11 +221,14 @@ public class HTablePoolTest {
 		private String tableName;
 		private String cfName;
 		private String cName;
-		public WriteHTableThread(HTablePool pool, String tableName, String columnFamilyName, String columnName) {
+		private ThreadsCompleteHandler handler;
+		public WriteHTableThread(HTablePool pool, String tableName, 
+				String columnFamilyName, String columnName, ThreadsCompleteHandler handler) {
 			this.pool = pool;
 			this.tableName = tableName;
 			this.cfName = columnFamilyName;
 			this.cName = columnName;
+			this.handler = handler;
 		}
 		@Override
 		public void run() {
@@ -182,9 +245,11 @@ public class HTablePoolTest {
 			} catch (Exception e) {
 				e.printStackTrace();
 				throw new RuntimeException(e);
+			} finally {
+				System.out.println("tName:" + tName + " ended with put:" + put);
+				this.pool.putTable(table);
+				this.handler.incrementCompletedThread();
 			}
-			System.out.println("tName:" + tName + " ended with put:" + put);
-			this.pool.putTable(table);
 		}
 	}
 	
